d14f532c6c383c8eee36d6332c83a394ba8c5f80
Halil İbrahim Koçoğlu <halilkocoglu98@gmail.com>
2023-02-02 12:17:51 +0300

    task delete function added
    -comment lines added
    -delete task button and function added

--- Javascript_ödev_2/todolist/js/app.js
+++ Javascript_ödev_2/todolist/js/app.js
@@ -1,22 +1,23 @@
-const TASK = document.querySelector('#task');
+var TASK = document.querySelector('#task');
 let ulEl = document.querySelector('#list');
 const toastEl = document.querySelector('#liveToast');
 const toastBody = document.querySelector('.toast-body');
 const toastImg = document.querySelector('#toast-img');
-let collapseBtn = `<button id="clsBtn"  type="button" class="close" 
+let collapseBtn = `<button data-delete="cls" class= "Btn rounded small border-secondary" type="button" class="close" 
 data-dismiss="alert" aria-label="Close">
-<span aria-hidden="true">&times;</span>
+<span class=" font-weight-bold" aria-hidden="true">&times;</span>
 </button>`
 let taskList = !localStorage.getItem('tasks') ?  [] : (localStorage.getItem('tasks').split(','));
 console.log(taskList)
 console.log(taskList.length)
+
 //get the list of tasks in local storage
 if(taskList.length>0){
     for (let i=0; i < taskList.length; i++) {
         if(checkForText(taskList[i])){
             TASK.value = taskList[i];
             const liDOM = document.createElement('li');
-            liDOM.innerHTML = `${TASK.value} ${collapseBtn}`;
+            liDOM.innerHTML = `<p>${TASK.value}</p> ${collapseBtn}`;
             liDOM.classList.add('list-group-item','d-flex', 
             'justify-content-between', 'align-items-center');
             liDOM.setAttribute('data-text',TASK.value);
@@ -34,12 +35,10 @@ function checkForText(text) {
         return found;       
 }
 //Add new Task
-function newElement (){
-    console.log(TASK.value);
-    console.log("running");
+async function newElement (){
     const liDOM = document.createElement('li');
-    if (TASK.value.length > 0){
-        if(checkForText(TASK.value)){
+    if (TASK.value.length > 0){     //if value length is greater than zero
+        if(checkForText(TASK.value)){   //if value is already in the list
             //toast
             toastBody.innerHTML = `<p class= "text-danger">
             Eklemek istediğiniz görev listede mevcut!!</p>`
@@ -50,8 +49,8 @@ function newElement (){
             $('#liveToast').toast('show');
             });
             TASK.value= '';
-        }else{
-            liDOM.innerHTML = `${TASK.value} ${collapseBtn}`;
+        }else{          //if value is not in the list, add it to the list
+            liDOM.innerHTML = `<p>${TASK.value}</p> ${collapseBtn}`;
             liDOM.classList.add('list-group-item','d-flex', 
             'justify-content-between', 'align-items-center');
             liDOM.setAttribute('data-text',TASK.value);
@@ -68,10 +67,13 @@ function newElement (){
             $(document).ready(function(){
                 $('#liveToast').toast('show');
             });
-
             TASK.value= '';
+            //working like sleep function
+            await new Promise(done => setTimeout(() => done(), 3000));
+            //reload page for close button - addEventListener(deleteTask)
+            location.reload();
         }
-    }else if (TASK.value.length <1 ){
+    }else if (TASK.value.length <1 ){   // if value length is smaller than one
         // toast
         toastBody.innerHTML = `<p class= "text-danger">Listeye boş ekleme yapamazsınız!!</p>`
         toastImg.innerHTML = `<img id="toastImg" style="width: 5px;" 
@@ -81,12 +83,26 @@ function newElement (){
             $('#liveToast').toast('show');
         });
         // toastEl.classList.replace('hide', 'show');
-        // await sleep (7000);
+        // await sleep (4000);
         // toastEl.classList.replace( 'show', 'hide');
         
     }
-
+    
+    
 }
 
 
 //Delete Task
+if(taskList.length > 0 ){
+    let clsBtn = document.querySelectorAll(`[data-delete="cls"]`)
+    let i = 0;
+    for(; i < clsBtn.length; i++){
+        clsBtn[i].addEventListener('click', deleteTask)
+    }
+    function deleteTask(){
+        let txt = this.parentElement.querySelector('p')
+        taskList.splice(taskList.indexOf(txt.innerText),1);
+        this.parentElement.remove();
+        localStorage.setItem('tasks',taskList);
+        }    
+}
